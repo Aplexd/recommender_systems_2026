7a20e35f2c1e1e0751311af92ff6d4abd1cf55c3
Jørgen Galdal <jorgen.4@live.no>
2026-02-16 17:25:25 +0100

    move loaders to utils

---
+++ utils.py
@@ -0,0 +1,34 @@
+import dotenv
+import polars as pl
+import os
+
+# Loads variables to environment
+dotenv.load_dotenv(".env")
+
+def _load_parquet(file_name: str) -> pl.DataFrame:
+    """
+    Loads a parquet-file
+    
+    :param file_name: Name of file in .env-file
+    :type file_name: str
+    """
+    path = os.getenv(file_name)
+    
+    if path is None:
+        raise NameError(f"No path specified in .env-file for {file_name}")
+
+    return pl.read_parquet(path)
+
+
+# Loads file content from environment
+def load_articles() -> pl.DataFrame:
+    return _load_parquet("ARTICLES")
+
+def load_embeddings() -> pl.DataFrame:
+    return _load_parquet("EMBEDDINGS")
+
+def load_behaviors() -> pl.DataFrame:
+    return _load_parquet("BEHAVIORS")
+
+def load_history() -> pl.DataFrame:
+    return _load_parquet("HISTORY")
